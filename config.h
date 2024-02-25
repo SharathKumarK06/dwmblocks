1274a5b2c51355753ddd5bd63e456aae9afae75c
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-02-25 12:13:31 +0530

    Make my initial changes and added 'statuscmd' patch
    Added blocks for statusbar
    'statuscmd' patch adds the ability to signal a status monitor program
    such as dwmblocks the location and button when clicking on the status
    bar.

---
+++ config.h
@@ -0,0 +1,12 @@
+/* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{" ",           "memory.sh",                            10,                     0},
+	{"",            "volume.sh",                             0,                     1},
+	{"",            "datetime.sh",                          60,                     0},
+	{"",            "battery.sh",                           30,                     0},
+};
+
+/* sets delimiter between status commands. NULL character ('\0') means no delimiter. */
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
