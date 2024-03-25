beada9de8780d718fc7b82bbbfd1b65e5941b3e6
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-03-25 13:48:50 +0530

    Add modules

--- config.h
+++ config.h
@@ -2,8 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
 	{" ",           "memory.sh",                            10,                     0},
+	{"",            "nettraffic.sh",                         1,                     0},
+	{"",            "internet.sh",                          10,                     0},
 	{"",            "volume.sh",                             0,                     1},
 	{"",            "datetime.sh",                          60,                     0},
+	{" ",          "switch_layout.sh",                     0,                      2},
 	{"",            "battery.sh",                           30,                     0},
 };
 
