33f958f0832be7be6c1923547a199c1d6fd3c9b9
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-07-04 08:47:52 -0700

    Add 'statuscmd' patch
    This patch allows to handle button interation on each modules in
    statusbar

--- config.h
+++ config.h
@@ -4,8 +4,7 @@
  */
 static const Block blocks[] = {
 	/*Icon*/    /*Command*/                                             /*Update Interval*/     /*Update Signal*/
-	{"Mem:",    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",      30,                     0},
-	{"",        "date '+%b %d (%a) %I:%M%p'",                                    5,                     0},
+	{"",        "battery.sh",                                                   1,                     1},
 };
 
 
