9f7fb92c1ef3516f6e9e4f1d5ed5c9d5307faaa5
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-07-06 18:10:15 +0530

    Add multiple modules

--- config.h
+++ config.h
@@ -1,10 +1,16 @@
-/*
- * modify this file to change what commands output to your statusbar, and
- * ecompile using the make command.
+/* modify this file to change what commands output to your statusbar, and
+ * recompile using the make command.
+ *
+ * run `kill -$(( 34 + signalnum )) $(pidof dwmblocks)` to update the dwmblocks
+ * status text
  */
 static const Block blocks[] = {
-	/*Icon*/    /*Command*/                                             /*Update Interval*/     /*Update Signal*/
-	{"",        "battery.sh",                                                   1,                     1},
+	/*Icon*/    /*Command*/                                     /*Update Interval*/     /*Update Signal*/
+	{"",        "volume.sh get",                                         0,                     5},
+	{"",        "backlight.sh get",                                      0,                     4},
+	{"",        "sb-date.sh",                                        86400,                     3},
+	{"",        "sb-time.sh",                                           60,                     2},
+	{"",        "sb-battery.sh",                                        60,                     1},
 };
 
 
